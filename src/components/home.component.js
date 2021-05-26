fe8b9e41c7f1624514d07b96bb81e1428b5bc04d
Ad <83498835+Howdy-admoll@users.noreply.github.com>
2021-05-26 23:52:08 +0100

    new commit

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,6 +1,18 @@
+import axios from "axios";
 import React, { Component } from "react";
 
 export default class Home extends Component{
+
+    componentDidMount(){
+        axios.get('user'). then(
+            res => {
+                console.log(res);
+            },
+            err => {
+                console.log(err)
+            }
+        )
+    }
 
     render(){
         return(
